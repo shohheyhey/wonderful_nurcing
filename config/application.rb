09ef11c04dca462012bb9ae6de01fed1e5862e39
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-09-07 08:33:37 +0900

    application.rbの設定 rails generate した時に作成されるファイルを設定。

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module WonderfulNurcing
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.templete_engine false
+      g.javascript false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec,
+                      view_specs: false,
+                      routing_specs: false,
+                      helper_specs: false,
+                      controller_specs: true,
+                      request_specs: true
+    end
+    
   end
 end
