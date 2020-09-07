279f10ea63a429e5b9a28bfdc21950c08c6c4386
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-09-07 17:09:03 +0900

    rubocop修正

--- app/models/office.rb
+++ app/models/office.rb
@@ -44,6 +44,6 @@ class Office < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8}
-  validates :password_confirmation, presence: true, length: { minimum: 8}
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true, length: { minimum: 8 }
 end
