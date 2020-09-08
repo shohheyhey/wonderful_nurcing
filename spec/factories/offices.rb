ce8f1c11d0aae6bdbfec99b908cc5c838748803b
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-09-08 11:53:06 +0900

    Officeモデルのバリデーションテストを実装 sequenceを再学習し、必須だなと感じた。 passwordとpassword_confirmationが一致しないとユーザー登録できないというプログラムは書いた覚えはないが、そうなっているということは devise_token_auth の仕様なんだろう。

---
+++ spec/factories/offices.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :office do
+    sequence(:name) {|n| "#{n}_#{Faker::Name.name}" }
+    sequence(:email) {|n| "#{n}_#{Faker::Internet.email}" }
+    sequence(:password) {|n| "#{n}_#{Faker::Internet.password}" }
+    password_confirmation { password }
+  end
+end
