83f374def628d6fd46a18e0fc6e5e09ffcf41bfa
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-09-13 20:50:11 +0900

    spec/controllers/apiv1/registrations_controller_spec.rcのtypeをrequestに修正

--- spec/controllers/api/v1/auth/registrations_controller_spec.rb
+++ spec/controllers/api/v1/auth/registrations_controller_spec.rb
@@ -1,19 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::Auth::RegistrationsController, type: :controller do
+RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
   fdescribe "POST api/v1/auth" do
-    subject { post("api/v1/auth", params) }
+    subject { post( api_v1_office_registration_path, params) }
     let(:params) do
-      {
-        "office":{
-            "name": "test_office",
-            "email": "test_office_email_26@example.com",
-            "password": "test_office_password",
-            "password_confirmation": "test_office_password"
-          }
-      }
-      end
-
+      { office: attributes_for(:office)}
+    end
     context "パラメータにname, email, password, password_confirmationが入力されているとき" do
       it "Officeの新規登録ができる。" do
         binding.pry
